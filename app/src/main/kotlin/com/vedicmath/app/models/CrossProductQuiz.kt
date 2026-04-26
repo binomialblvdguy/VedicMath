202ef34651d804307a7cde1d4241c01b82f177fa
j <binomialblvdguy@gmail.com>
2026-04-26 06:30:36 -0500

    Expand models CrossProductQuiz wrapper coverage

--- app/src/main/kotlin/com/vedicmath/app/models/CrossProductQuiz.kt
+++ app/src/main/kotlin/com/vedicmath/app/models/CrossProductQuiz.kt
@@ -34,6 +34,46 @@ object CrossProductQuiz {
         )
     }
 
+    fun createUnitsSum4TensStep1Item(
+        smallerTens: Int,
+        smallerUnits: Int
+    ): CrossProductQuizItem {
+        return com.vedicmath.app.domain.models.CrossProductQuiz.createUnitsSum4TensStep1Item(
+            smallerTens = smallerTens,
+            smallerUnits = smallerUnits
+        )
+    }
+
+    fun createUnitsSum6TensStep1Item(
+        smallerTens: Int,
+        smallerUnits: Int
+    ): CrossProductQuizItem {
+        return com.vedicmath.app.domain.models.CrossProductQuiz.createUnitsSum6TensStep1Item(
+            smallerTens = smallerTens,
+            smallerUnits = smallerUnits
+        )
+    }
+
+    fun createUnitsSum9TensStep1Item(
+        smallerTens: Int,
+        smallerUnits: Int
+    ): CrossProductQuizItem {
+        return com.vedicmath.app.domain.models.CrossProductQuiz.createUnitsSum9TensStep1Item(
+            smallerTens = smallerTens,
+            smallerUnits = smallerUnits
+        )
+    }
+
+    fun createUnitsSum11TensStep1Item(
+        smallerTens: Int,
+        smallerUnits: Int
+    ): CrossProductQuizItem {
+        return com.vedicmath.app.domain.models.CrossProductQuiz.createUnitsSum11TensStep1Item(
+            smallerTens = smallerTens,
+            smallerUnits = smallerUnits
+        )
+    }
+
     fun createUnitsSum15TensStep1Item(
         smallerTens: Int,
         smallerUnits: Int
@@ -41,6 +81,36 @@ object CrossProductQuiz {
         return com.vedicmath.app.domain.models.CrossProductQuiz.createUnitsSum15TensStep1Item(
             smallerTens = smallerTens,
             smallerUnits = smallerUnits
+        )
+    }
+
+    fun createUnitsSum8TensDiff2Item(
+        smallerTens: Int,
+        smallerUnits: Int
+    ): CrossProductQuizItem {
+        return com.vedicmath.app.domain.models.CrossProductQuiz.createUnitsSum8TensDiff2Item(
+            smallerTens = smallerTens,
+            smallerUnits = smallerUnits
+        )
+    }
+
+    fun createDigits1To2RatioRightVerticalItem(
+        leftBase: Int,
+        rightBase: Int
+    ): CrossProductQuizItem {
+        return com.vedicmath.app.domain.models.CrossProductQuiz.createDigits1To2RatioRightVerticalItem(
+            leftBase = leftBase,
+            rightBase = rightBase
+        )
+    }
+
+    fun createDigits2To1RatioLeftVerticalItem(
+        leftBase: Int,
+        rightBase: Int
+    ): CrossProductQuizItem {
+        return com.vedicmath.app.domain.models.CrossProductQuiz.createDigits2To1RatioLeftVerticalItem(
+            leftBase = leftBase,
+            rightBase = rightBase
         )
     }
 
